cfc6a7608d01c33f72669db83be36dc79f4c08e4
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-07-31 10:49:41 +0300

    Fix Login

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './Registration.css';
 
 export default function Registration() {
+    const [state, setState] = useState('Login');
+
     return (
         <div className="registration">
             <div className="registration-container">
-                <h1>Sign Up</h1>
+                <h1>{state}</h1>
                 <div className="registration-fields">
-                    <input type="text" name="name" placeholder="Your name" />
+                    {state === 'Sign Up' 
+                        ? <input type="text" name="name" placeholder="Your name" />
+                        : <></>
+                    }
                     <input type="email" name="email" placeholder="Email address" />
                     <input type="password" name="password" placeholder="Password" />
                 </div>
                 <button>Continue</button>
-                <p className="registration-login">Already have an account? <span>Login here</span></p>
+                {state === 'Sign Up' 
+                    ? <p className="registration-login">Already have an account? <span onClick={() => setState('Login')}>Login here</span></p>
+                    : <p className="registration-login">Create an account <span onClick={() => setState('Sign Up')}>Click here</span></p>
+                }
                 <div className="registration-agree">
                     <input type="checkbox" name="" id="" />
                     <p>By continuing, I agree to the terms of use & privacypolicy.</p>
